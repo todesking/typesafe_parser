18425b7b1d839ffe5a50f077d26d63252a85336e
todesking <discommunicative@gmail.com>
2020-09-20 13:22:48 +0900

    Constant parser

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,7 +1,25 @@
-import {head} from "./"
+import {Parser, parse, constant} from "./"
 
-test('head', () => {
-  const a: 1 = head([1, 2, 3] as const)
-  expect(head([1,2,3])).toBe(1)
-  expect(() => head([])).toThrow()
+function str(s: string): string { return s }
+
+function eq<T>(actual: T, expected: T): void {
+  expect(actual).toStrictEqual(expected)
+}
+
+test('constatnt', () => {
+  const parser = constant('foo', 1 as const)
+
+  const ok1: [1, ''] = parse(parser, 'foo')
+  eq(ok1, [1, ''])
+
+  const ok2: [1, '!!!'] = parse(parser, 'foo!!!')
+  eq(ok2, [1, '!!!'])
+
+  const parser2: Parser<1> = constant('foo', 1)
+  const ok3: [1, string] = parse(parser2, 'foo!!!')
+  eq(ok3, [1, '!!!'])
+
+  expect(() => {
+    const ng: never = parse(parser, 'boo')
+  }).toThrow()
 })
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,37 @@
-export type Head<T extends readonly unknown[]> = T[0]
-
-export function head<T extends readonly unknown[]>(t: T): Head<T> {
-  if(t.length == 0) throw '!!!'
-  return t[0]
+export type Constant<P extends string, V> = {
+  type: 'constant',
+  pattern: P,
+  value: V
 }
 
+export function constant<P extends string, V>(p: P, v: V): Constant<P, V> {
+  return {
+    type: 'constant',
+    pattern: p,
+    value: v
+  }
+}
+
+export type Parser<T> =
+  Constant<string, T>
+
+export type Parse<T, P extends Parser<T>, S extends string> =
+  P extends Constant<infer P, infer V> ?
+    S extends `${P}${infer Rest}` ? [V, Rest] :
+    string extends S ? [V, string] : never
+  : [T, string]
+
+function parse_error(s: string): never {
+  throw '!!!'
+}
+
+export function parse<T, P extends Parser<T>, S extends string>(p: P, s: S): Parse<T, P, S> {
+  const generic_parser: Parser<T> = p
+  switch(generic_parser.type) {
+    case 'constant':
+      if(s.startsWith(generic_parser.pattern)) {
+        return [generic_parser.value, s.substr(generic_parser.pattern.length)] as any
+      }
+      parse_error(s)
+  }
+}
